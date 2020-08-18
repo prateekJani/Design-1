5d89643de088a86d94b1c33a198cfebe904d3034
prateekJani <58832930+prateekJani@users.noreply.github.com>
2020-08-17 20:56:27 -0400

    Create MinStack.java

---
+++ MinStack.java
@@ -0,0 +1,46 @@
+public class MinStack {
+    int arr[];
+    int top1;
+    
+    public MinStack() {
+        arr = new int[214748364];
+        top1 = -1;
+    }
+        
+    public void push(int x) {
+        top1++;
+        arr[top1]=x;
+
+    }
+        
+    public void pop() {
+        arr[top1] = 0;
+        top1--;
+    }
+        
+    public int top() {
+        return arr[top1];
+    }
+        
+    public int getMin() {
+        int minValue = arr[0]; 
+        for(int i=1;i<top1+1;i++){ 
+          if(arr[i] < minValue){ 
+            minValue = arr[i]; 
+          } 
+        } 
+        return minValue; 
+    }
+
+    public static void main(String args[]){
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin()); // return -3
+        minStack.pop();
+        System.out.println(minStack.top()); // return 0
+        System.out.println(minStack.getMin()); // return -2
+    }
+
+}
